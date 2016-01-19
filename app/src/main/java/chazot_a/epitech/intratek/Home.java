4cc2d8b5a7097dc28dae905329befcec9fffb13a
sebastien cache-delanos <sebastien.cache-delanos@epitech.eu>
2016-01-19 12:29:11 +0100

    Update

--- app/src/main/java/chazot_a/epitech/intratek/Home.java
+++ app/src/main/java/chazot_a/epitech/intratek/Home.java
@@ -1,7 +1,5 @@
-package intratek.intratek;
+package chazot_a.epitech.intratek;
 
-import android.app.AlertDialog;
-import android.content.DialogInterface;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
@@ -15,15 +13,26 @@ import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
 
+import intratek.intratek.R;
+
 public class Home extends AppCompatActivity
         implements NavigationView.OnNavigationItemSelectedListener {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_home);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+
+        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();
+            }
+        });
 
         DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
         ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
@@ -48,7 +57,7 @@ public class Home extends AppCompatActivity
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.main, menu);
+        getMenuInflater().inflate(R.menu.home, menu);
         return true;
     }
 
@@ -73,42 +82,18 @@ public class Home extends AppCompatActivity
         // Handle navigation view item clicks here.
         int id = item.getItemId();
 
-        AlertDialog.Builder dialog = new AlertDialog.Builder(this);
-        dialog.setTitle("Menu button pressed");
-        dialog.setPositiveButton(android.R.string.yes, new DialogInterface.OnClickListener() {
-            public void onClick(DialogInterface dialog, int which) {
-                dialog.dismiss();
-            }
-        });
-        dialog.setIcon(android.R.drawable.ic_dialog_alert);
+        if (id == R.id.nav_camara) {
+            // Handle the camera action
+        } else if (id == R.id.nav_gallery) {
 
-        if (id == R.id.nav_home) {
-            dialog.setMessage("Home page requested");
-            dialog.show();
-        } else if (id == R.id.nav_profile) {
-            dialog.setMessage("Profile page requested");
-            dialog.show();
-        } else if (id == R.id.nav_activities) {
-            dialog.setMessage("Activities page requested");
-            dialog.show();
-        } else if (id == R.id.nav_planning) {
-            dialog.setMessage("Planning page requested");
-            dialog.show();
-        } else if (id == R.id.nav_grades) {
-            dialog.setMessage("Grades page requested");
-            dialog.show();
-        } else if (id == R.id.nav_projects) {
-            dialog.setMessage("Projects page requested");
-            dialog.show();
-        } else if (id == R.id.nav_messages) {
-            dialog.setMessage("Messages page requested");
-            dialog.show();
-        } else if (id == R.id.nav_about) {
-            dialog.setMessage("About page requested");
-            dialog.show();
-        } else if (id == R.id.nav_disconnect) {
-            dialog.setMessage("Disconnect page requested");
-            dialog.show();
+        } else if (id == R.id.nav_slideshow) {
+
+        } else if (id == R.id.nav_manage) {
+
+        } else if (id == R.id.nav_share) {
+
+        } else if (id == R.id.nav_send) {
+
         }
 
         DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
--- app/src/main/java/chazot_a/epitech/intratek/Launcher.java
+++ app/src/main/java/chazot_a/epitech/intratek/Launcher.java
@@ -1,45 +1,51 @@
-package intratek.intratek;
+package chazot_a.epitech.intratek;
 
 import android.content.Intent;
 import android.content.SharedPreferences;
-import android.os.Handler;
 import android.preference.PreferenceManager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.os.Handler;
+import android.view.Window;
+import android.view.WindowManager;
 
+import intratek.intratek.R;
+
+/**
+ * Launcher activity of Intratek.
+ * It's a fullscreen splash screen.
+ */
 public class Launcher extends AppCompatActivity {
 
     private Runnable    _runnable;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        this.requestWindowFeature(Window.FEATURE_NO_TITLE);
+        this.getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_launcher);
 
         SharedPreferences preferences = PreferenceManager.getDefaultSharedPreferences(this);
         boolean isConnected = preferences.getBoolean("isConnected", false);
-        boolean challengeStatus = preferences.getBoolean("ChallengeStatus", false);
 
-        if (!isConnected)
+        if (!isConnected) //Not Connected
         {
-            _runnable = new Runnable()
-            {
+            _runnable = new Runnable() {
                 @Override
-                public void run()
-                {
+                public void run() {
                     Intent intent = new Intent(Launcher.this, Login.class);
                     startActivity(intent);
                     finish();
                 }
             };
-        }
-        else //Connected
+        } else //Connected
         {
             _runnable = new Runnable() {
                 @Override
                 public void run() {
-                    Intent intent = new Intent(Launcher.this, Home.class);
-                    startActivity(intent);
+                    //Intent intent = new Intent(Launcher.this, Home.class);
+                    //startActivity(intent);
                     finish();
                 }
             };
