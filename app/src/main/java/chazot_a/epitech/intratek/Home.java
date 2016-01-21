401fe8df9ed3ac4b99f21b98ec3dc5ca693b893d
sebastien cache-delanos <sebastien.cache-delanos@epitech.eu>
2016-01-21 17:18:21 +0100

    Update

--- app/src/main/java/chazot_a/epitech/intratek/Home.java
+++ app/src/main/java/chazot_a/epitech/intratek/Home.java
@@ -1,12 +1,7 @@
 package chazot_a.epitech.intratek;
 
-import android.app.AlertDialog;
-import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
-import android.view.View;
 import android.support.design.widget.NavigationView;
 import android.support.v4.view.GravityCompat;
 import android.support.v4.widget.DrawerLayout;
