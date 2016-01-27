6ad4a5c3c3e61617c0321a388de9fe1c80e81b9e
jordan chazottes <jordan.chazottes@epitech.eu>
2016-01-27 17:36:06 +0100

    debug grades

---
+++ app/src/main/java/epitech/intratek/json/Planning.java
@@ -0,0 +1,46 @@
+package epitech.intratek.json;
+
+/**
+ * Created by Dardaxe on 27/01/2016.
+ */
+public class Planning
+{
+    //Prof prof_inst;
+    public String title;
+    public String rdv_indic_registered;
+    public String allowed_planning_end;
+    public float nb_group;
+    public String start;
+    public String register_month;
+    public String allowed_planning_start;
+    public boolean project;
+    public String event_registered;
+    public float total_students_registered;
+    public boolean allow_register;
+    public String codemodule;
+    public String rdv_group_registered;
+    public float semester;
+    public String type_code;
+    public String is_rdv;
+    public boolean allow_token;
+    public String titlemodule;
+    public boolean in_more_than_one_month;
+    public String acti_title;
+    public String instance_location;
+    public String nb_hours;
+    public String register_prof;
+    public String nb_max_student_projet;
+    public Room room;
+    public String codeacti;
+    public String codeevent;
+    public String codeinstance;
+    public String dates;
+    public boolean register_student;
+    public String type_title;
+    public float num_event;
+    public String end;
+    public String scolaryear;
+    public boolean module_registered;
+    public boolean past;
+    public boolean module_available;
+}
---
+++ app/src/main/java/epitech/intratek/json/Room.java
@@ -0,0 +1,11 @@
+package epitech.intratek.json;
+
+/**
+ * Created by Dardaxe on 27/01/2016.
+ */
+public class Room
+{
+    public String type;
+    public float seats;
+    public String code;
+}
